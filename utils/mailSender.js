5aefa3801b7e1e0f085a14cf50d1a8c9516bad90
mateen-mahi <mateenmahihot@gmail.com>
2026-06-20 03:03:44 +0500

    mail Sender changing

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -7,8 +7,8 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   // host: "smtp.gmail.com",
-  // port: 587,
-  // secure: false, 
+  port: 587,
+  secure: false, 
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASS,
