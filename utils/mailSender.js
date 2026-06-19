20b071cb2ce17cd2ac49bf06fe1a87ffccd899fa
mateen-mahi <mateenmahihot@gmail.com>
2026-06-20 02:44:33 +0500

    new Page added OTP again

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -13,6 +13,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASS,
   },
+    tls: {
+    rejectUnauthorized: false
+  }
 });
 
 const baseLayout = (title, content) => `
