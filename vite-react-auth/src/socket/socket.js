a3adc4492bc6acdca9a3a702b2af06cab919fde3
mateen-mahi <mateenmahihot@gmail.com>
2026-07-21 04:26:30 +0500

    latest commit

--- vite-react-auth/src/socket/socket.js
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-
-const socket = io("https://complete-auth-in-express-main.onrender.com", {
-  withCredentials: true,
-  autoConnect: false,
-  transports: ["websocket"],
-});
-
-export default socket;
